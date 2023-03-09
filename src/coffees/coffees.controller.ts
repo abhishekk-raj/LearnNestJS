1d2691ccf3ae135a6447ae408e61b050c7b5c19c
Abhishek Raj <abhishek@betatestsolutions.com>
2023-03-09 21:38:29 +0530

    Create dynamic path with params

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('flavor')
   findAll() {
     return 'This will return all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This will return coffee #${id}`;
   }
 }
