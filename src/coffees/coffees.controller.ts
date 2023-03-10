63b230490d7fade7d8a27f0221a4b62e1dfe8625
Abhishek Raj <abhishek@betatestsolutions.com>
2023-03-10 11:09:44 +0530

    Update and delete operations

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -15,5 +23,15 @@ export class CoffeesController {
   @Post()
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This will update coffee #${id}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `This will delete coffee #${id}`;
   }
 }
