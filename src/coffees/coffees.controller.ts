52c3d3818bae1ec075e4ecfbd47c5262344f0186
Abhishek Raj <abhishek@betatestsolutions.com>
2023-03-11 16:59:24 +0530

    Add validation to transform request body to dto type.

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -29,6 +29,8 @@ export class CoffeesController {
 
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    console.log(createCoffeeDto instanceof CreateCoffeeDto);
+
     return this.coffeesService.create(createCoffeeDto);
   }
 
