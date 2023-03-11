de1acf45556074d18bad0fa22a66886e81ad26f4
Abhishek Raj <abhishek@betatestsolutions.com>
2023-03-11 14:02:41 +0530

    Create service and update controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,32 +8,35 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) { }
+
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This will return all coffees. Limit ${limit}, Offset ${offset}`;
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This will return coffee #${id}`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This will update coffee #${id}`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return `This will delete coffee #${id}`;
+    return this.coffeesService.remove(id);
   }
 }
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+    private coffees: Coffee[] = [
+        {
+            id: 1,
+            name: "Shipwreck Roast",
+            brand: "Buddy Brew",
+            flavor: ["chocolate", "vanilla"]
+        }
+    ];
+
+    findAll() {
+        return this.coffees;
+    }
+
+    findOne(id: string) {
+        return this.coffees.find((item) => item.id === +id);
+    }
+
+    create(createCoffeeDto: any) {
+        this.coffees.push(createCoffeeDto);
+        return this.coffees;
+    }
+
+    update(id: string, updateCoffeeDto: any) {
+        const existingCoffee = this.findOne(id);
+        if (existingCoffee) {
+            // Update the coffee
+        }
+    }
+
+    remove(id: string) {
+        const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+        if (coffeeIndex >= 0) {
+            this.coffees.splice(coffeeIndex, 1);
+        }
+    }
+}
