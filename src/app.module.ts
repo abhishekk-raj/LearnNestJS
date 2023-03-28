ae71906e60d6cd3a45a26901e4fc531ee7eec18d
Abhishek Raj <abhishek@betatestsolutions.com>
2023-03-28 11:56:14 +0530

    Control nest module encapsulation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { dataSourceOptions } from 'ormconfig';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
     CoffeesModule,
+    CoffeeRatingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
