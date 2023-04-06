d948369b8594738066c9a016bb4cf150dadcfaf1
Abhishek Raj <abhishek@betatestsolutions.com>
2023-04-06 20:38:55 +0530

    Revert class provider demo code.

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,20 +7,12 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
-class ConfigService { }
-class DevelopmentConfigService { }
-class ProductionConfigService { }
-
 
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
     providers: [
         CoffeesService,
-        {
-            provide: ConfigService,
-            useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService
-        },
         { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }
     ],
     exports: [CoffeesService]
