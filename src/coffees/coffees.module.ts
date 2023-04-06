875eaa1254e13d86f51fc8a980ab291614a87345
Abhishek Raj <abhishek@betatestsolutions.com>
2023-04-06 20:47:25 +0530

    Factory provider simple example

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Injectable, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from '../events/entities/event.entity';
 import { CoffeesController } from './coffees.controller';
@@ -7,20 +7,13 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
-/* It's just a psudo code */
-@Injectable()
-export class CoffeeBrandFactory {
-    create() {
-        return ['buddy brew', 'nescafe'];
-    }
-}
 
 @Module({
     imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController],
     providers: [
         CoffeesService,
-        { provide: COFFEE_BRANDS, useFactory: (brandFactory: CoffeeBrandFactory) => brandFactory.create(), inject: [CoffeeBrandFactory] }
+        { provide: COFFEE_BRANDS, useFactory: () => ['buddy brew', 'nescafe'] }
     ],
     exports: [CoffeesService]
 })
