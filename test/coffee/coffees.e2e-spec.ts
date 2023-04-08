193cf2aa977189d88957661bd8da38b3c61b1ff1
Abhishek Raj <abhishek@betatestsolutions.com>
2023-04-08 19:27:04 +0530

    Implementing e2e test logic

--- test/coffee/coffees.e2e-spec.ts
+++ test/coffee/coffees.e2e-spec.ts
@@ -1,10 +1,18 @@
-import { INestApplication } from "@nestjs/common";
+import { HttpStatus, INestApplication, ValidationPipe } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import { CoffeesModule } from "../../src/coffees/coffees.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { CreateCoffeeDto } from "src/coffees/dto/create-coffee.dto";
+import * as request from 'supertest';
 
 
 describe('[Feature] Coffees - /coffees', () => {
+    const coffee = {
+        name: 'Shipwreck Roast',
+        brand: 'Buddy Brew',
+        flavors: ['chocolate', 'vanilla']
+    };
+
     let app: INestApplication;
 
     beforeAll(async () => {
@@ -24,10 +32,33 @@ describe('[Feature] Coffees - /coffees', () => {
         }).compile();
 
         app = moduleFixture.createNestApplication();
+        app.useGlobalPipes(new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true,
+            transformOptions: {
+                enableImplicitConversion: true
+            }
+        }));
         await app.init();
     });
 
-    it.todo('Create [POST /]');
+    it('Create [POST /]', () => {
+        return request(app.getHttpServer())
+            .post('/coffees')
+            .send(coffee as unknown as CreateCoffeeDto)
+            .expect(HttpStatus.CREATED)
+            .then(({ body }) => {
+                const expectedCoffee = jasmine.objectContaining({
+                    ...coffee,
+                    flavors: jasmine.arrayContaining(
+                        coffee.flavors.map(name => jasmine.objectContaining({ name }))
+                    ),
+                });
+                expect(body).toEqual(expectedCoffee);
+            });
+    });
+
     it.todo('Get All [GET /]');
     it.todo('Get One [GET /:id]');
     it.todo('Update One [PATCH /:id]');
