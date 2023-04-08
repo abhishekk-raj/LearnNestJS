f4f3fbee60aafc00b6c376d1d2bf9d08629a3007
Abhishek Raj <abhishek@betatestsolutions.com>
2023-04-08 13:16:25 +0530

    Logging with middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key/api-key.guard';
+import { LoggingMiddleware } from './middleware/logging/logging.middleware';
 
 @Module({
     imports: [ConfigModule],
     providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }]
 })
-export class CommonModule { }
+export class CommonModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggingMiddleware).forRoutes('*');
+    }
+}
